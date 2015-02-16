252007cbb384fc2f612bff790afcee18380417e9
Server <calumgathergood@gmail.com>
2015-02-16 10:26:06 +0000

    Test Commit, changed problem file to mno.6

--- Computational_Intelligence_CW/src/set10107/Main.java
+++ Computational_Intelligence_CW/src/set10107/Main.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 public class Main {
 
 	public static void main(String[] args) {
-		Problem.loadProblem("Problem Files/Problem1.txt");
+		Problem.loadProblem("Problem Files/Problem5.txt");
 		
 		Job[] myJobs = Problem.getJobs();
 		
