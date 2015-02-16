ea94aae709055a02a541eff4d3a7602a9da21f99
Server <calumgathergood@gmail.com>
2015-02-16 09:54:11 +0000

    Added Inital files from moodle. Created main class and seperate 'Problem Files' folder.

---
+++ Computational_Intelligence_CW/src/set10107/Job.java
@@ -0,0 +1,9 @@
+package set10107;
+
+public class Job {
+    int id;
+	int pickup;
+	int setdown;
+	int available;
+	int[][] payments = new int[4][2];
+}
---
+++ Computational_Intelligence_CW/src/set10107/Main.java
@@ -0,0 +1,24 @@
+package set10107;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Problem.loadProblem("Problem Files/Problem1.txt");
+		
+		Job[] myJobs = Problem.getJobs();
+		
+		Collections.shuffle(Arrays.asList(myJobs));
+		
+		System.out.println("Delivery order ");
+		for(Job j : myJobs){
+			System.out.println(j.id + ", ");
+		}
+		System.out.println();
+		System.out.println("Reward =" +Problem.score(myJobs));
+
+	}
+
+}
