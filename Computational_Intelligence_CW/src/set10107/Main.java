b67577484f4f5d4a2a9af15dadb01ea80b49bb34
cgathergood <calumgathergood@gmail.com>
2015-02-16 10:56:04 +0000

    Configured eclipse and git on Ubuntu. Changed the problem file number again.

--- Computational_Intelligence_CW/src/set10107/Main.java
+++ Computational_Intelligence_CW/src/set10107/Main.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 public class Main {
 
 	public static void main(String[] args) {
-		Problem.loadProblem("Problem Files/Problem5.txt");
+		Problem.loadProblem("Problem Files/Problem4.txt");
 		
 		Job[] myJobs = Problem.getJobs();
 		
