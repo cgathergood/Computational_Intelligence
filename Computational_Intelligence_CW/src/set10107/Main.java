f62ba31e2af1ead3f4d4298b41c1360bdfd22e4b
cgathergood <calumgathergood@gmail.com>
2015-03-09 11:54:35 +0000

    Starting out with A*. Problem with Ubuntu so I'm switching to Windows :(

--- Computational_Intelligence_CW/src/set10107/Main.java
+++ Computational_Intelligence_CW/src/set10107/Main.java
@@ -8,6 +8,7 @@
 
 package set10107;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
@@ -19,16 +20,55 @@ public class Main {
 		Job[] myJobs = Problem.getJobs();
 		
 		//Pint out Job Data
-		for(Job j : myJobs){
+/*		for(Job j : myJobs){
 			System.out.println("ID: " + j.id + ", Pickup:" + j.pickup + ", Setdown:" + j.setdown + ", Available:" + j.available);
 			
 			for(int[] i : j.payments){
 				System.out.println("Payments: " +i[0] + "," +i[1]);
 			}
+		}*/
+		
+		// A Star
+		
+		int time;
+		int shortest = 100;
+		
+		ArrayList<Job> openList = new ArrayList<Job>();
+		ArrayList<Job> closedList = new ArrayList<Job>();
+		
+		// Populate openList
+		for(Job j : myJobs){
+			openList.add(j);
 		}
 		
+		Job currentJob;
 		
-		Collections.shuffle(Arrays.asList(myJobs));
+		while(openList.isEmpty() == false){
+			currentJob = openList.get(0);
+			if(currentJob == myJobs[0]){
+				return;
+			} else {
+				closedList.add(currentJob);
+				openList.remove(currentJob);
+			}
+			System.out.println(openList.size());
+		}
+		
+/*		for(Job j : myJobs){
+			for(Job neighbour : myJobs){
+				time = Problem.getTime(j.setdown, neighbour.setdown);
+				//System.out.println("From "+ j.id + " to " + neighbour.id + " = " + time);
+				if(time < shortest && time != 0){
+					shortest = time;
+				}
+			}
+			System.out.println("Shortest time: " + shortest);
+			shortest = 100;
+		}
+		System.out.println("Reward =" +Problem.score(myJobs));*/
+		
+		// Random Shuffle
+		//Collections.shuffle(Arrays.asList(myJobs));
 		
 		System.out.println("Delivery order ");
 		for(Job j : myJobs){
