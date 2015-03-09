3e33327b34eecffabefadbd10f9c4ddf2e4604b4
Calum Gathergood <calumgathergood@gmail.com>
2015-03-09 12:40:07 +0000

    Not much progress. Need to invesitgate the Ubuntu bug. Focus on sorting the open list first then think about optimisation.

--- Computational_Intelligence_CW/src/set10107/Main.java
+++ Computational_Intelligence_CW/src/set10107/Main.java
@@ -14,69 +14,66 @@ import java.util.Collections;
 
 public class Main {
 
-	public static void main(String[] args) { 
-		Problem.loadProblem("Problem Files/Problem1.txt"); //Load Problem File
-		
+	public static void main(String[] args) {
+		Problem.loadProblem("Problem Files/Problem1.txt"); // Load Problem File
+
 		Job[] myJobs = Problem.getJobs();
-		
-		//Pint out Job Data
-/*		for(Job j : myJobs){
-			System.out.println("ID: " + j.id + ", Pickup:" + j.pickup + ", Setdown:" + j.setdown + ", Available:" + j.available);
-			
-			for(int[] i : j.payments){
-				System.out.println("Payments: " +i[0] + "," +i[1]);
-			}
-		}*/
-		
+
+		// Pint out Job Data
+		/*
+		 * for(Job j : myJobs){ System.out.println("ID: " + j.id + ", Pickup:" +
+		 * j.pickup + ", Setdown:" + j.setdown + ", Available:" + j.available);
+		 * 
+		 * for(int[] i : j.payments){ System.out.println("Payments: " +i[0] +
+		 * "," +i[1]); } }
+		 */
+
 		// A Star
-		
+
 		int time;
 		int shortest = 100;
-		
+
 		ArrayList<Job> openList = new ArrayList<Job>();
 		ArrayList<Job> closedList = new ArrayList<Job>();
-		
+
 		// Populate openList
-		for(Job j : myJobs){
+		for (Job j : myJobs) {
 			openList.add(j);
 		}
-		
-		Job currentJob;
-		
-		while(openList.isEmpty() == false){
-			currentJob = openList.get(0);
-			if(currentJob == myJobs[0]){
-				return;
-			} else {
-				closedList.add(currentJob);
-				openList.remove(currentJob);
-			}
-			System.out.println(openList.size());
+
+		Job currentJob = myJobs[0];
+
+		// while (openList.isEmpty() == false) {
+		closedList.add(currentJob);
+		openList.remove(currentJob);
+
+		// Order open list
+		for (Job j : openList) {
+			System.out.println("The distance from " + currentJob.id + " to "
+					+ j.id + " is "
+					+ Problem.getTime(currentJob.setdown, j.pickup));
 		}
-		
-/*		for(Job j : myJobs){
-			for(Job neighbour : myJobs){
-				time = Problem.getTime(j.setdown, neighbour.setdown);
-				//System.out.println("From "+ j.id + " to " + neighbour.id + " = " + time);
-				if(time < shortest && time != 0){
-					shortest = time;
-				}
-			}
-			System.out.println("Shortest time: " + shortest);
-			shortest = 100;
-		}
-		System.out.println("Reward =" +Problem.score(myJobs));*/
-		
+		// return;
+		// }
+
+		/*
+		 * for (Job j : myJobs) { for (Job neighbour : myJobs) { time =
+		 * Problem.getTime(j.setdown, neighbour.pickup);
+		 * System.out.println("From "+ j.id + " to " + neighbour.id + " = "
+		 * +time); if (time < shortest && time != 0) { shortest = time; } }
+		 * System.out.println("Shortest time: " + shortest); shortest = 100; }
+		 * System.out.println("Reward =" + Problem.score(myJobs));
+		 */
+
 		// Random Shuffle
-		//Collections.shuffle(Arrays.asList(myJobs));
-		
+		// Collections.shuffle(Arrays.asList(myJobs));
+
 		System.out.println("Delivery order ");
-		for(Job j : myJobs){
+		for (Job j : myJobs) {
 			System.out.println(j.id + ", ");
 		}
 		System.out.println();
-		System.out.println("Reward =" +Problem.score(myJobs));
+		System.out.println("Reward =" + Problem.score(myJobs));
 
 	}
-
 }
