57ae8defa0ca42ee8dfa0126dd5a6eff1285dd17
cgathergood <calumgathergood@gmail.com>
2015-03-19 11:55:39 +0000

    Changed createChromo() so that each new chromosone is random. Now creates a population of chromosones. Next step is to implement crossover function.

--- Computational_Intelligence_CW/src/set10107/EvolutionaryAlgorithm.java
+++ Computational_Intelligence_CW/src/set10107/EvolutionaryAlgorithm.java
@@ -1,5 +1,9 @@
 package set10107;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class EvolutionaryAlgorithm {
@@ -8,18 +12,24 @@ public class EvolutionaryAlgorithm {
 
 		Problem.loadProblem("Problem Files/Problem1.txt");
 		Job[] myJobs = Problem.getJobs();
-		
+
 		int tries = 0;
 		int attempt = 10000;
 		
+		// Creates a population of chromosones
+		int populationSize = 100;
+		List<int[]> population = createPopulation(populationSize, myJobs);
+
 		int[] chromo = createChromo(myJobs);
 		int bestFit = fitness(myJobs, chromo);
 
 		while (tries < attempt) {
 
-			int[] newChromo = chromo;			
-			
+			int[] newChromo = chromo;
+
 			newChromo = mutate(newChromo);
+
+			// newChromo = crossover(newChromo);
 
 			int newFit = fitness(myJobs, newChromo);
 
@@ -27,27 +37,57 @@ public class EvolutionaryAlgorithm {
 				chromo = newChromo;
 				bestFit = newFit;
 
-				System.out.println("Improved fitness is now \t " +  bestFit + "\t on try \t " + tries );
+				System.out.println("Improved fitness is now \t " + bestFit
+						+ "\t on try \t " + tries);
 			}
 			tries++;
 		}
-		
-		for(int i : chromo){
+
+		for (int i : chromo) {
 			System.out.println(i + ",");
 		}
-		
-		System.out.println("Final Fitness= " +bestFit);
-		
+
+		System.out.println("Final Fitness= " + bestFit);
+
 	}
 
-	// Creates basic chromosone
+	// Creates random chromosone
 	public static int[] createChromo(Job[] jobs) {
 
 		int[] chromo = new int[jobs.length];
 		for (int i = 0; i < jobs.length; i++) {
 			chromo[i] = i;
 		}
+
+		chromo = shuffle(chromo);
+
 		return chromo;
+	}
+
+	// Shuffle method
+	public static int[] shuffle(int[] a) {
+		int n = a.length;
+		for (int i = 0; i < n; i++) {
+			// between i and n-1
+			int r = i + (int) (Math.random() * (n - i));
+			int tmp = a[i]; // swap
+			a[i] = a[r];
+			a[r] = tmp;
+		}
+		return a;
+	}
+
+	// Creates a population of chromosones
+	public static List<int[]> createPopulation(int size, Job[] jobs) {
+
+		List<int[]> population = new ArrayList<int[]>();
+
+		for (int i = 0; i < size; i++) {
+			int[] chromo = createChromo(jobs);
+			population.add(chromo);
+		}
+		System.out.println("Population Created!");
+		return population;
 	}
 
 	// Mutates the chromosone
@@ -59,18 +99,24 @@ public class EvolutionaryAlgorithm {
 
 		int temp1 = chromo[random1];
 		int temp2 = chromo[random2];
-		
-		chromo[random1]=temp2;
+
+		chromo[random1] = temp2;
 		chromo[random2] = temp1;
-		
+
 		return chromo;
+	}
+
+	// Single point crossover
+	private static int[] crossover(int[] newChromo) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	// Calculates the fitness of the chromosone
 	private static int fitness(Job[] myJobs, int[] chromo) {
 		Job[] sol = new Job[myJobs.length];
 		for (int i = 0; i < myJobs.length; i++) {
-			sol[i] = myJobs[chromo[i]];			
+			sol[i] = myJobs[chromo[i]];
 		}
 		return Problem.score(sol);
 	}
